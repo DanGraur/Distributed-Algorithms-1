7dddbc75f73b230c93302954db83392afa24a70e
Graur Dan <dangraur1@yahoo.com>
2017-11-16 15:59:42 +0100

    Initial Test Commit

--- src/main/java/simple_test/server/ServerClass.java
+++ src/main/java/simple_test/server/ServerClass.java
@@ -14,6 +14,12 @@ public class ServerClass {
     private final static String PATH = "rmi://localhost:5555/add_class";
 
     public static void main(String[] args) throws Exception {
+        if (System.getSecurityManager() == null) {
+            System.setProperty("java.security.policy","src/main/resources/generic.policy");
+
+            System.setSecurityManager(new SecurityManager());
+        }
+
         Registry registry = LocateRegistry.createRegistry(5555);
 
         MyRemoteInterface implementingClass = new MyImplementingClass();
